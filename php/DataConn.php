c1b1e379fb4cad4b412d145e3296071b0040df76
serranocjm <cserrano@teravisiontech.com>
2019-04-29 11:33:02 -0400

    Add connection parameters

--- php/DataConn.php
+++ php/DataConn.php
@@ -9,13 +9,29 @@
 class DataConn
 {
     private $host = "localhost";
-    private $user;
-    private $password;
-    private $db;
+    private $post = "8089";
+    private $user = "user";
+    private $password = "password";
+    private $db = "tictactoe";
+    private $charset = "utf8mb4";
 
     private $conn;
+    private $dsn;
+    private $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
 
+    private function __construct()
+    {
+        $this->dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
 
-
+        try {
+            $this->conn = new PDO($this->dsn, $this->user, $this->password, $this->options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
 
 }
