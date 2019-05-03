195245ad71169dd14c652d51b69ef44bef969339
serranocjm <serrano.cjm@gmail.com>
2019-05-02 23:47:35 -0400

    adjust jquery

--- js/main.js
+++ js/main.js
@@ -1,43 +1,45 @@
 $(document).ready(function () {
-    function clickCell(cellContent) {
-        console.log(jQuery.trim(cellContent));
-        getLatestMatch();
+    function clickCell(cellContent, cellVal) {
+        cellVal.val(cellContent);
+        console.log(jQuery.trim(cellContent),cellVal.val());
+
+        //getLatestMatch();
     }
 
     $("#td_a1").on('click', function () {
-        clickCell($("#cell_a1_value").text());
+        clickCell($("#p_a1").text(), $('input[name="cell_a1"]'));
     });
 
     $("#td_a2").on('click', function () {
-        clickCell($("#cell_a2_value").text());
+        clickCell($("#p_a2").text(), $('input[name="cell_b1"]'));
     });
 
     $("#td_a3").on('click', function () {
-        clickCell($("#cell_a3_value").text());
+        clickCell($("#p_a3").text(), $('input[name="cell_c1"]'));
     });
 
     $("#td_b1").on('click', function () {
-        clickCell($("#cell_b1_value").text());
+        clickCell($("#p_b1").text(), $('input[name="cell_a2"]'));
     });
 
     $("#td_b2").on('click', function () {
-        clickCell($("#cell_b2_value").text());
+        clickCell($("#p_b2").text(), $('input[name="cell_b2"]'));
     });
 
     $("#td_b3").on('click', function () {
-        clickCell($("#cell_b3_value").text());
+        clickCell($("#p_b3").text(), $('input[name="cell_c2"]'));
     });
 
     $("#td_c1").on('click', function () {
-        clickCell($("#cell_c1_value").text());
+        clickCell($("#p_c1").text(), $('input[name="cell_a3"]'));
     });
 
     $("#td_c2").on('click', function () {
-        clickCell($("#cell_c2_value").text());
+        clickCell($("#p_c2").text(), $('input[name="cell_b3"]'));
     });
 
     $("#td_c3").on('click', function () {
-        clickCell($("#cell_c3_value").text());
+        clickCell($("#p_c3").text(), $('input[name="cell_c3"]'));
     });
 
     $("#btn_new_game").on('click', function () {
@@ -65,8 +67,35 @@ $(document).ready(function () {
         });
     }
 
-    function fillBoard(data) {
+    function fillMatchData(data) {
+        //CELDAS
+        $("#p_a1").text(data.cell_a1);
+        $("#p_b1").text(data.cell_b1);
+        $("#p_c1").text(data.cell_c1);
+        $("#p_a2").text(data.cell_a2);
+        $("#p_b2").text(data.cell_b2);
+        $("#p_c2").text(data.cell_c2);
+        $("#p_a3").text(data.cell_a3);
+        $("#p_b3").text(data.cell_b3);
+        $("#p_c3").text(data.cell_c3);
+        $("#current_player").text(data.current_player);
+        $("#current_round").text(data.match_round);
+        $("#winner_h").text(data.match_winner);
 
+        //FORMULARIO
+        $('input[name="cell_a1"]').val(data.cell_a1);
+        $('input[name="cell_b1"]').val(data.cell_b1);
+        $('input[name="cell_c1"]').val(data.cell_c1);
+        $('input[name="cell_a2"]').val(data.cell_a2);
+        $('input[name="cell_b2"]').val(data.cell_b2);
+        $('input[name="cell_c2"]').val(data.cell_c2);
+        $('input[name="cell_a3"]').val(data.cell_a3);
+        $('input[name="cell_b3"]').val(data.cell_b3);
+        $('input[name="cell_c3"]').val(data.cell_c3);
+        $('input[name="match_round"]').val(data.match_round);
+        $('input[name="match_status"]').val("IN_PROGRESS");
+        $('input[name="match_winner"]').val(data.match_winner);
+        $('input[name="current_player"]').val(data.current_player);
     }
 
     function newGame() {
@@ -76,7 +105,12 @@ $(document).ready(function () {
             dataType: ' json',
             data: $("form").serialize(),
             success: function (data) {
-                console.log("success", data)
+                if(data != null){
+                    console.log("success", data)
+                    fillMatchData(data);
+                }else {
+                    alert("ERROR!")
+                }
             },
             error: function (error) {
                 console.log("error", error)
@@ -91,6 +125,13 @@ $(document).ready(function () {
             dataType: ' json',
             data: $("form").serialize(),
             success: function (data) {
+                if(data != null){
+                    console.log("success", data)
+                    fillMatchData(data);
+                }else {
+                    alert("ERROR!")
+                }
+
                 console.log("success", data)
             },
             error: function (error) {
